dd2259165e1fe0e59e42382bb419370ea513020f
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-12-10 22:18:54 +0700

    add steps definitions feature registration

---
+++ src/test/java/starter/stepsdefinition/RegistrasiSteps.java
@@ -0,0 +1,9 @@
+package starter.stepsdefinition;
+
+import io.cucumber.java.en.Given;
+
+public class RegistrasiSteps {
+    @Given("I am open registration page in barengin")
+    public void iAmOpenRegistrationPageInBarengin() {
+    }
+}
