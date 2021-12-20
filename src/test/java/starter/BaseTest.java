abcaf4a97b66499df8530613919beac0b0425641
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-12-20 18:43:43 +0700

    add base test

--- src/test/java/starter/BaseTest.java
+++ src/test/java/starter/BaseTest.java
@@ -1,7 +1,9 @@
 package starter;
 
+import starter.pages.LoginPage;
 import starter.pages.RegistrationPage;
 
 public class BaseTest {
     public RegistrationPage registration = new RegistrationPage();
+    public LoginPage login = new LoginPage();
 }
