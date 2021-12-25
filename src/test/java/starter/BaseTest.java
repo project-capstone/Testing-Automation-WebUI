ff4f43ade713be4c7cbf2c0703c60f2e88c2a5e1
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-12-25 21:47:49 +0700

    update basetest

--- src/test/java/starter/BaseTest.java
+++ src/test/java/starter/BaseTest.java
@@ -1,9 +1,11 @@
 package starter;
 
 import starter.pages.LoginPage;
+import starter.pages.ProductPage;
 import starter.pages.RegistrationPage;
 
 public class BaseTest {
     public RegistrationPage registration = new RegistrationPage();
     public LoginPage login = new LoginPage();
+    public ProductPage product = new ProductPage();
 }
