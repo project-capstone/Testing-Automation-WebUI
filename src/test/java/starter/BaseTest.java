73f66d670a687d7ea1e352358f8fac2a9fbb359b
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-12-27 23:37:02 +0700

    update

--- src/test/java/starter/BaseTest.java
+++ src/test/java/starter/BaseTest.java
@@ -1,15 +1,23 @@
 package starter;
 
+import starter.pages.GroupProduct.CreateGroupProduct;
+import starter.pages.GroupProduct.DeleteGroupProduct;
+import starter.pages.GroupProduct.UpdateGroupProduct;
 import starter.pages.LoginPage;
+import starter.pages.LogoutPage;
 import starter.pages.Product.CreateProduct;
+import starter.pages.Product.DeleteProduct;
 import starter.pages.Product.UpdateProduct;
-import starter.pages.ProductPage;
 import starter.pages.RegistrationPage;
 
 public class BaseTest {
     public RegistrationPage registration = new RegistrationPage();
     public LoginPage login = new LoginPage();
-    public ProductPage product = new ProductPage();
+    public LogoutPage logout = new LogoutPage();
     public CreateProduct products = new CreateProduct();
     public UpdateProduct updateproduct = new UpdateProduct();
+    public DeleteProduct deleteProduct = new DeleteProduct();
+    public CreateGroupProduct creategroupproduct = new CreateGroupProduct();
+    public DeleteGroupProduct deletegroupproduct = new DeleteGroupProduct();
+    public UpdateGroupProduct updategroupproduct = new UpdateGroupProduct();
 }
